d60dc3cd3f70d3f6d5bb9c6707cf0874acf7f149
Admin <linhlv@galaxy.com.vn>
2023-01-05 11:05:39 +0700

    ICANID

--- src/auth-state.tsx
+++ src/auth-state.tsx
@@ -1,4 +1,4 @@
-import { User } from '@auth0/icanid-sdk-spa-js';
+import { User } from '@icanid/icanid-sdk-spa-js';
 
 /**
  * The auth state which, when combined with the auth methods, make up the return object of the `useICANID` hook.
